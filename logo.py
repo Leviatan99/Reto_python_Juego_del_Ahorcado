2b91b74f2e5af5c842b652cbb80cb8ea46833439
Leviatan99 <danielcubs23@gmail.com>
2021-09-20 18:28:16 -0500

    Introduccion y funciones de lectura del archico data.txt

---
+++ logo.py
@@ -0,0 +1,4 @@
+introduction = open ('ahorcado3.txt','r')
+mensaje = introduction.read()
+print(mensaje)
+introduction.close()
---
+++ main.py
@@ -0,0 +1,38 @@
+#Este es un programa para cumplir con el reto final del curso de python interm
+import random
+import os
+def read_data():
+    words = []
+    with open('./data.txt','r',encoding='utf-8') as f:
+        for line in f:
+            words.append(line)
+    new_words = [line[:-1] for line in words]
+
+    return new_words
+
+
+def read_introduction():
+    introduction = open ('ahorcado3.txt','r')
+    mensaje = introduction.read()
+    print(mensaje)
+    introduction.close()    
+
+
+def select_word(data_set):
+    return random.choice(data_set)
+    
+
+def run():
+    data_set = read_data()
+    #print(data_set)
+
+    print(read_introduction())
+
+    word_to_guess = select_word(data_set)
+    #
+    # print(word_to_guess)
+
+
+    
+if __name__ =='__main__':
+    run()
