3090c550140ea9d349882e1263a4a38e75696146
Leviatan99 <danielcubs23@gmail.com>
2021-10-03 14:18:53 -0500

    Se grego el textode mensaje de ganador

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-#Este es un programa para cumplir con el reto final del curso de python interm
+#Este es un programa para cumplir con el reto final del curso de python intermedio
 import random
 import os
+import unidecode
+
 def read_data():
     words = []
     with open('./data.txt','r',encoding='utf-8') as f:
@@ -14,25 +16,50 @@ def read_data():
 def read_introduction():
     introduction = open ('ahorcado3.txt','r')
     mensaje = introduction.read()
-    print(mensaje)
-    introduction.close()    
+    introduction.close()
+    return mensaje    
 
 
 def select_word(data_set):
     return random.choice(data_set)
-    
+
+
+def normalize(data_Set):
+    new_data_set = [unidecode.unidecode(word) for word in data_Set]
+    return new_data_set
+
+
+def upper_word(word_to_guess):
+    word_upper = word_to_guess.upper()
+    return word_upper
+
+
+def word_slices(word_to_guess):
+    if len(word_to_guess) > 4 :
+        word_to_print_1 = word_to_guess[0]
+        word_to_print_2 = word_to_guess[4]
+        word_to_print_3 = word_to_guess[-1]
+
+def win():
+    winning_message = open ('win_1.txt','r')
+    mensaje = winning_message.read()
+    winning_message.close()
+    return mensaje
 
 def run():
     data_set = read_data()
-    #print(data_set)
-
     print(read_introduction())
-
-    word_to_guess = select_word(data_set)
-    palabra = random.choice(word_to_guess)
+    data_set = normalize(data_set)
     
-    print(word_to_guess)
-    print(palabra)
+    tuplas_list_data_Set = list(enumerate(data_set))
+    tupla_word_to_guess = select_word(tuplas_list_data_Set)
+    word_to_guess = tupla_word_to_guess[1]
+    
+    print( upper_word( word_to_guess))
+    
+    #print(win())
+
+
     
 if __name__ =='__main__':
     run()
